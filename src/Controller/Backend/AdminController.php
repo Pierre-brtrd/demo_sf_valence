fe0e194cc5b4fa036f86d781758d3e462ff8a9d9
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2022-07-13 12:09:36 +0200

    feat: Add Article Type and vue admin article

---
+++ src/Controller/Backend/AdminController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Controller\Backend;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class Admin Controller
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * Article repository to find article object
+     * 
+     * @var ArticleRepository
+     */
+    private $repoArticle;
+
+    /**
+     * User repository to find user object
+     * 
+     * @var UserRepository
+     */
+    private $repoUser;
+
+    /**
+     * Entity manager interface
+     *
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(ArticleRepository $repoArticle, UserRepository $repoUser, EntityManagerInterface $em)
+    {
+        $this->repoArticle = $repoArticle;
+        $this->repoUser = $repoUser;
+        $this->em = $em;
+    }
+
+    #[Route('', name: 'admin')]
+    public function index(): Response
+    {
+        // Récupérer tout les users
+        $users = $this->repoUser->findAll();
+
+        // Récupérer tout les articles
+        $articles = $this->repoArticle->findAll();
+
+        return $this->render('Backend/index.html.twig', [
+            'articles' => $articles,
+            'users' => $users,
+        ]);
+    }
+
+    #[Route('/article/create', name: 'admin.article.create')]
+    public function createArticle(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($article);
+            $this->em->flush();
+            $this->addFlash('success', 'Article créé avec succès');
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('Backend/Article/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/article/edit/{id}', name: 'admin.article.edit', methods: 'GET|POST')]
+    public function editArticle($id, Request $request)
+    {
+        $article = $this->repoArticle->find($id);
+
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($article);
+            $this->em->flush();
+            $this->addFlash('success', 'Article modifié avec succès');
+            return $this->redirectToRoute('admin');
+        }
+
+        return $this->render('Backend/Article/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Form/ArticleType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
